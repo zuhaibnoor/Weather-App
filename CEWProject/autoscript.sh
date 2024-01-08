3cce26cf6da4448049413c5076f48c227554e203
Muhammad Owais <138030370+MuhammadOwais03@users.noreply.github.com>
2024-01-08 22:24:34 +0500

    Autoscript version

---
+++ CEWProject/autoscript.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Set the working directory
+cd /home/owais/Desktop/CEW-OEL
+
+# Compile the C program
+gcc headers/cJSON.c Functions/read.c Functions/retrieval.c Functions/date_utils.c test.c -o run -lcurl
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Weather app compiled successfully"
+else
+    echo "Weather app compilation failed"
+    exit 1
+fi
+
+# Set the choice parameter
+choice="$1"
+
+# Ensure the executable is not busy
+while fuser ./run &>/dev/null; do
+    echo "Waiting for ./run to be available..."
+    sleep 1
+done
+
+echo "Choice $choice"
+echo "Weather app executing successfully"
+
+case $choice in
+    1)
+        # echo "Retrieving..."
+        ./run $choice
+        ;;
+    2)
+        # echo "Data has been processed"
+        result=$(./run $choice)
+        # echo "Result: $result"
+        if [ "$result" -eq 1 ]; then
+            chmod +x send_email_script.sh
+            ./send_email_script.sh
+        #else
+        #    echo "NO ANOMALY"
+        fi
+        ;;
+    3)
+        # echo "Report Generating..."
+        ./run $choice
+        ;;
+    *)
+        clear
+        # echo "Invalid choice"
+        ;;
+esac
+
+echo "Weather app executed successfully"
