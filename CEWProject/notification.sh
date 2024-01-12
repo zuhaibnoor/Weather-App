ad0d5c7950cfdb5f90d83ec2556411096b006046
zuhaibnoor <zuhaibnoor5@gmail.com>
2024-01-13 00:53:59 +0500

    Updated system alert system

--- CEWProject/notification.sh
+++ CEWProject/notification.sh
@@ -4,6 +4,6 @@ file_to_monitor="Anamoly.log"  # Replace with the actual path to your file
 
 inotifywait -m -e modify,attrib,close_write,move,create,delete "$file_to_monitor" |
 while read -r directory events filename; do
-    notify-send "Anamoly in weather status detected!" "Check $filename File for new anamolies."
+    notify-send "Anamoly in weather status detected!" "Check $filename File for new anamolies now!."
     
 done &  # Run the script in the background
