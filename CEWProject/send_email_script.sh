f932137549f96d8bf512cfde1c174e4a2e03d64d
zuhaibnoor <zuhaibnoor5@gmail.com>
2024-01-13 01:46:13 +0500

    Updated the Email system

--- CEWProject/send_email_script.sh
+++ CEWProject/send_email_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 recipient_email="owais4500107@cloud.neduet.edu.pk"
-subject="I AM SENDING THIS FROM LINUX"
+subject="Warning! Weather anamoly detected!"
 attachment="Anamoly.log"
 
 # Check if uuencode is installed
@@ -11,7 +11,7 @@ if ! command -v uuencode &> /dev/null; then
 fi
 
 # Send email with attachment
-(echo "TEST FROM OWAIS" && uuencode "$attachment" "$attachment") | mail -s "$subject" "$recipient_email"
+(echo "Dear User, this is an automatically generated Email. You are recieving this mail because your weather app has detected an anamoly in the weather status. To stay safe and prevent any mishaps please have a look at the attached log file for more information. STAY SAFE ! Thank you" && uuencode "$attachment" "$attachment") | mail -s "$subject" "$recipient_email"
 
 if [ $? -eq 0 ]; then
     echo "Email sent successfully."
